7ad9f6e62037dab718b0c525526ef6c74ae2cf26
silviapineda <silviapinedasanjuan@gmail.com>
2017-02-08 10:18:32 -0800

    Create function ProcessSample

--- CloneInference.py
+++ CloneInference.py
@@ -59,25 +59,11 @@ def ObtainNumberClones(ListClones):
                                     'number':count},ignore_index=True)
         count=count+1
     return df_clones
-    
-########################
-###### Main program ####
-########################
 
-data_clonesInference = pandas.read_csv("/Users/Pinedasans/Data/VDJ/data_clonesInference.txt",sep="\t")
 
-###Obtain the unique subjects
-sample_unique = data_clonesInference['sample_id'].unique()
-
-##Declare result: total number of clones infered per subject
-result_ClonesInfered = pd.DataFrame([])
-
-###Loop for each subject
-for i in range(0,len(sample_unique)):
-    print (i)
-    data_clonesInference_sample_unique = data_clonesInference[data_clonesInference['sample_id'] == sample_unique[i]]
-    ##To calculate the minimum read depth per individual to calculate the clones
+def ProcessSample(data_clonesInference_sample_unique):
     min_read_depth = min(pd.Series(data_clonesInference['sample_id']).value_counts())
+    ##To calculate the minimum read depth per individual to calculate the clones
     data_clonesInference_sample_unique_down = data_clonesInference_sample_unique.sample(min_read_depth) 
     V_J_CDR3_unique = data_clonesInference_sample_unique_down['V_J_lenghCDR3'].unique()
     ClonesInfered = 0
@@ -91,14 +77,29 @@ for i in range(0,len(sample_unique)):
         ##Obtain the number of clones infered per sample
         ClonesInfered = ObtainNumberClones(ProcessNucleotides(nucleotides))
         result.loc[data_clonesInference_V_J_CDR3_unique.index,'numberClone'] = pd.Series(ClonesInfered['number']).values
-        
-    result_ClonesInfered = result_ClonesInfered.append(result)  
+    return result
+    
+########################
+###### Main program ####
+########################
+
+data_clonesInference = pd.read_csv("/Users/Pinedasans/Data/VDJ/data_clonesInference.txt",sep="\t")
+
+###Obtain the unique subjects
+sample_unique = data_clonesInference['sample_id'].unique()
+
+##Declare result: total number of clones infered per subject
+result_ClonesInfered = pd.DataFrame([])
+
+###Loop for each subject
+rangeSamples = range(0,len(sample_unique))
+###Loop for each subject
+for i in range(0,len(sample_unique)):
+    print (i)
+    data_clonesInference_sample_unique = data_clonesInference[data_clonesInference['sample_id'] == sample_unique[i]]
+    final_result = ProcessSample(data_clonesInference_sample_unique)
+      
+    result_ClonesInfered = result_ClonesInfered.append(final_result)  
 
 ###Result    
 result_ClonesInfered.to_csv('/Users/Pinedasans/Data/VDJ/ClonesInfered_downsampled.csv')
-
-
-
-
-
-
