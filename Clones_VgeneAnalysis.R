d7a6ec5890e04c5c2458e9485df9d3b70d73279a
silviapineda <silviapinedasanjuan@gmail.com>
2017-06-16 18:07:49 -0700

    First commit of the v_gene analysis

---
+++ Clones_VgeneAnalysis.R
@@ -0,0 +1,183 @@
+rm(list = ls(all = TRUE))
+x<-date()
+print(x)
+###########################################################################################
+### PROJECT: Immune Repertoire. Analysis B cells antibodies
+###
+### CITATION: 
+###
+### PROCESS: 
+###           
+### DESCRIP: Analysis of the VDJ data
+###         
+###
+### Author: Silvia Pineda
+### Date: June, 2017
+############################################################################################
+library(ggplot2)
+library(untb)
+library(lme4)
+library("caroline")
+library("effects")
+library("pheatmap")
+
+
+setwd("/Users/Pinedasans/VDJ/Results/")
+load("/Users/Pinedasans/VDJ/Data/VDJ_order.Rdata")
+
+##### V gene usage plots ####
+
+############
+### gDNA ###
+############
+data_qc_gDNA<-data_qc[which(data_qc$amplification_template=="gDNA"),]
+#data_qc_cDNA<-data_qc[which(data_qc$amplification_template=="cDNA"),]
+
+### longitudinal
+data_qc_gDNA_long<-data_qc_gDNA[which(data_qc_gDNA$clin!="AR" & data_qc_gDNA$clin!="pre-AR"),]
+reads_clones_annot_Long<-reads_clones_annot[which(reads_clones_annot$clin!="AR" & reads_clones_annot$clin!="pre-AR"),]
+reads_clones_annot_Long_qc<-reads_clones_annot_Long[which(reads_clones_annot_Long$reads_gDNA>100),]
+
+id<-match(data_qc_gDNA_long$specimen_label,reads_clones_annot_Long_qc$specimen_id)
+data_qc_gDNA_long_qc<-data_qc_gDNA_long[which(is.na(id)==F),]
+data_qc_gDNA_long_qc$specimen_label<-factor(data_qc_gDNA_long_qc$specimen_label)
+
+v_gene_df<-data.frame(table(data_qc_gDNA_long_qc$v_gene,data_qc_gDNA_long_qc$specimen_label))
+colnames(v_gene_df)<-c("v_gene","specimen","count")
+v_gene_df$clin = reads_clones_annot_Long_qc[v_gene_df$specimen,1] 
+v_gene_df$time = reads_clones_annot_Long_qc[v_gene_df$specimen,4] 
+v_gene_df$sample = reads_clones_annot_Long_qc[v_gene_df$specimen,2]
+v_gene_df$clones = reads_clones_annot_Long_qc[v_gene_df$specimen,43]
+v_gene_df$freq<-v_gene_df$count/v_gene_df$clones
+
+g1<-ggplot(v_gene_df[which(v_gene_df$clin=="NP"),], aes(x=time,y=freq,group=v_gene,fill=v_gene)) +
+  scale_x_continuous(breaks = c(0,6,12,24,32)) + geom_area(aes(fill=v_gene)) + theme(legend.position="none") + 
+  scale_y_continuous(limit = c(0,1)) + facet_grid(clin ~ sample) + labs(x = "time", y = "V gene usage")
+
+g2<-ggplot(v_gene_df[which(v_gene_df$clin=="PNR"),], aes(x=time,y=freq,group=v_gene,fill=v_gene)) +
+  scale_x_continuous(breaks = c(0,6,12,24,32)) + geom_area(aes(fill=v_gene)) + theme(legend.position="none") + 
+  scale_y_continuous(limit = c(0,1)) + facet_grid(clin ~ sample) + labs(x = "time", y = "V gene usage")
+
+g3<-ggplot(v_gene_df[which(v_gene_df$clin=="PR"),], aes(x=time,y=freq,group=v_gene,fill=v_gene)) +
+  scale_x_continuous(breaks = c(0,6,12,24,32)) + geom_area(aes(fill=v_gene)) + theme(legend.position="none") + 
+  scale_y_continuous(limit = c(0,1)) + facet_grid(clin ~ sample) + labs(x = "time", y = "V gene usage")
+
+tiff("v_gene_usage_gDNA_long.tiff",res=300,h=1700,w=2700)
+multiplot(g1,g2,g3)
+dev.off()
+
+
+### AR
+data_qc_gDNA_AR<-data_qc_gDNA[which(data_qc_gDNA$clin=="AR" | data_qc_gDNA$clin=="pre-AR"),]
+reads_clones_annot_AR<-reads_clones_annot[which(reads_clones_annot$clin=="AR" | reads_clones_annot$clin=="pre-AR"),]
+reads_clones_annot_AR_qc<-reads_clones_annot_AR[which(reads_clones_annot_AR$reads_gDNA>100),]
+
+id<-match(data_qc_gDNA_AR$specimen_label,reads_clones_annot_AR_qc$specimen_id)
+data_qc_gDNA_AR_qc<-data_qc_gDNA_AR[which(is.na(id)==F),]
+data_qc_gDNA_AR_qc$specimen_label<-factor(data_qc_gDNA_AR_qc$specimen_label)
+
+v_gene_df<-data.frame(table(data_qc_gDNA_AR_qc$v_gene,data_qc_gDNA_AR_qc$specimen_label))
+colnames(v_gene_df)<-c("v_gene","specimen","count")
+v_gene_df$clin = reads_clones_annot_AR_qc[v_gene_df$specimen,1] 
+v_gene_df$time = reads_clones_annot_AR_qc[v_gene_df$specimen,4] 
+v_gene_df$sample = reads_clones_annot_AR_qc[v_gene_df$specimen,2]
+v_gene_df$clones = reads_clones_annot_AR_qc[v_gene_df$specimen,43]
+v_gene_df$reads = reads_clones_annot_AR_qc[v_gene_df$specimen,35]
+v_gene_df$clin<-relevel(v_gene_df$clin,ref="pre-AR")
+xx<-aggregate(v_gene_df$count, by=list(v_gene=v_gene_df$v_gene), FUN=sum)
+v_gene_df$total = xx[v_gene_df$v_gene,2]
+v_gene_df$freq<-v_gene_df$count/v_gene_df$total
+
+tiff("v_gene_usage_gDNA_AR.tiff",res=300,h=1700,w=2700)
+ggplot(v_gene_df, aes(x=clin,y=freq,group=v_gene,fill=v_gene)) +
+  geom_area(aes(fill=v_gene)) + theme(legend.position="none") + 
+  facet_grid(~ sample) + labs( y = "V gene usage")
+dev.off()
+
+
+############
+### cDNA ###
+############
+data_qc_cDNA<-data_qc[which(data_qc$amplification_template=="cDNA"),]
+
+### longitudinal
+data_qc_cDNA_long<-data_qc_cDNA[which(data_qc_cDNA$clin!="AR" & data_qc_cDNA$clin!="pre-AR"),]
+reads_clones_annot_Long<-reads_clones_annot[which(reads_clones_annot$clin!="AR" & reads_clones_annot$clin!="pre-AR"),]
+reads_clones_annot_Long_qc<-reads_clones_annot_Long[which(reads_clones_annot_Long$reads_cDNA>100),]
+
+id<-match(data_qc_cDNA_long$specimen_label,reads_clones_annot_Long_qc$specimen_id)
+data_qc_cDNA_long_qc<-data_qc_cDNA_long[which(is.na(id)==F),]
+data_qc_cDNA_long_qc$specimen_label<-factor(data_qc_cDNA_long_qc$specimen_label)
+
+v_gene_df<-data.frame(table(data_qc_cDNA_long_qc$v_gene,data_qc_cDNA_long_qc$specimen_label))
+colnames(v_gene_df)<-c("v_gene","specimen","count")
+v_gene_df$clin = reads_clones_annot_Long_qc[v_gene_df$specimen,1] 
+v_gene_df$time = reads_clones_annot_Long_qc[v_gene_df$specimen,4] 
+v_gene_df$sample = reads_clones_annot_Long_qc[v_gene_df$specimen,2]
+v_gene_df$clones = reads_clones_annot_Long_qc[v_gene_df$specimen,42]
+
+
+g1<-ggplot(v_gene_df[which(v_gene_df$clin=="NP"),], aes(x=time,y=count/clones,group=v_gene,fill=v_gene)) +
+  scale_x_continuous(breaks = c(0,6,12,24,32)) + geom_area(aes(fill=v_gene)) + theme(legend.position="none") + 
+  scale_y_continuous(limit = c(0,300000)) + facet_grid(clin ~ sample) + labs(x = "time", y = "V gene usage")
+
+g2<-ggplot(v_gene_df[which(v_gene_df$clin=="PNR"),], aes(x=time,y=count/clones,group=v_gene,fill=v_gene)) +
+  scale_x_continuous(breaks = c(0,6,12,24,32)) + geom_area(aes(fill=v_gene)) + theme(legend.position="none") + 
+  scale_y_continuous(limit = c(0,300000)) + facet_grid(clin ~ sample) + labs(x = "time", y = "V gene usage")
+
+g3<-ggplot(v_gene_df[which(v_gene_df$clin=="PR"),], aes(x=time,y=count/clones,group=v_gene,fill=v_gene)) +
+  scale_x_continuous(breaks = c(0,6,12,24,32)) + geom_area(aes(fill=v_gene)) + theme(legend.position="none") + 
+  scale_y_continuous(limit = c(0,300000)) + facet_grid(clin ~ sample) + labs(x = "time", y = "V gene usage")
+
+tiff("v_gene_usage_cDNA_long.tiff",res=300,h=1700,w=2700)
+multiplot(g1,g2,g3)
+dev.off()
+
+
+### AR
+data_qc_cDNA_AR<-data_qc_cDNA[which(data_qc_cDNA$clin=="AR" | data_qc_cDNA$clin=="pre-AR"),]
+reads_clones_annot_AR<-reads_clones_annot[which(reads_clones_annot$clin=="AR" | reads_clones_annot$clin=="pre-AR"),]
+reads_clones_annot_AR_qc<-reads_clones_annot_AR[which(reads_clones_annot_AR$reads_cDNA>100),]
+
+id<-match(data_qc_cDNA_AR$specimen_label,reads_clones_annot_AR_qc$specimen_id)
+data_qc_cDNA_AR_qc<-data_qc_cDNA_AR[which(is.na(id)==F),]
+data_qc_cDNA_AR_qc$specimen_label<-factor(data_qc_cDNA_AR_qc$specimen_label)
+
+v_gene_df<-data.frame(table(data_qc_cDNA_AR_qc$v_gene,data_qc_cDNA_AR_qc$specimen_label))
+colnames(v_gene_df)<-c("v_gene","specimen","count")
+v_gene_df$clin = reads_clones_annot_AR_qc[v_gene_df$specimen,1] 
+v_gene_df$time = reads_clones_annot_AR_qc[v_gene_df$specimen,4] 
+v_gene_df$sample = reads_clones_annot_AR_qc[v_gene_df$specimen,2]
+v_gene_df$clin<-relevel(v_gene_df$clin,ref="pre-AR")
+
+tiff("v_gene_usage_cDNA_AR.tiff",res=300,h=1700,w=2700)
+ggplot(v_gene_df, aes(x=clin,y=count,group=v_gene,fill=v_gene)) +
+  geom_area(aes(fill=v_gene)) + theme(legend.position="none") + 
+  facet_grid(~ sample) + labs(x = "time", y = "V gene usage")
+dev.off()
+
+
+###### V-gene usage analysis ####
+data_qc_gDNA$specimen_label<-factor(data_qc_gDNA$specimen_label)
+v_gene_matrix<-table(data_qc_gDNA$v_gene,data_qc_gDNA$specimen_label)
+reads_clones_annot_Long<-reads_clones_annot[which(reads_clones_annot$clin!="AR" & reads_clones_annot$clin!="pre-AR"),]
+id<-match(reads_clones_annot_Long$specimen_id,colnames(v_gene_matrix))
+v_gene_matrix_long<-v_gene_matrix[,na.omit(id)]
+id2<-match(colnames(v_gene_matrix_long),reads_clones_annot_Long$specimen_id)
+
+p.value<-NULL
+for (i in 1:nrow(v_gene_matrix_long)){
+  result<-anova(lm(v_gene_matrix_long[i,]~reads_clones_annot_Long$clin[id2]))
+  p.value[i]<-result$`Pr(>F)`[1]
+}
+p.adj<-p.adjust(p.value,method = "fdr")
+v_gene_matrix_long_sign<-v_gene_matrix_long[which(p.adj<0.1),]
+
+annot_col<-data.frame(reads_clones_annot_Long$clin[id2])
+rownames(annot_col)<-colnames(v_gene_matrix_long_sign)
+colnames(annot_col)<-"clin"
+pheatmap(v_gene_matrix_long_sign,scale = "row",border_color = NA,annotation_col = annot_col )
+
+
+
+
