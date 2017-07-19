91a0eebb922d11e57fa8d2d44106e9e91c401fc6
silviapineda <silviapinedasanjuan@gmail.com>
2017-07-19 13:51:14 -0700

    adding plots

--- PlotSummaryVDJ.R
+++ PlotSummaryVDJ.R
@@ -22,37 +22,47 @@ library(ggplot2)
 library(devtools)
 library(easyGgplot2)
 
-setwd("/Users/Pinedasans/VDJ/")
-load("/Users/Pinedasans/VDJ/Data/VDJ.Rdata")
+setwd("/Users/Pinedasans/VDJ/Results/")
+load("/Users/Pinedasans/VDJ/Data/VDJ_order.Rdata")
 
 
 ####Plot reads by gDNA 
 counts_gDNA <- reads_clones_annot$reads_gDNA[which(reads_clones_annot$reads_gDNA!=0)]
 names(counts_gDNA) <-reads_clones_annot$specimen_id[which(reads_clones_annot$reads_gDNA!=0)]
 cols = c("firebrick3","chartreuse4","dodgerblue3","darkorange2","goldenrod")[reads_clones_annot$clin[which(reads_clones_annot$reads_gDNA!=0)]]
+
+tiff("barplot_reads_by_gDNA.tiff",res=300,w=4000,h=2000)
 barplot(counts_gDNA,col=cols,main="Number of Reads gDNA",xlab = "Samples", ylab = "Reads",las=2)
+abline(h=100)
 legend(0, 20000, legend=levels(reads_clones_annot$clin[which(reads_clones_annot$reads_gDNA!=0)]),
        col=c("firebrick3","chartreuse4","dodgerblue3","darkorange2","goldenrod"),pch=15, cex=0.8)
+dev.off()
 
 ####Plot reads by cDNA 
 counts_cDNA <- reads_clones_annot$reads_cDNA[which(reads_clones_annot$reads_cDNA!=0)]
 names(counts_cDNA) <-reads_clones_annot$specimen_id[which(reads_clones_annot$reads_cDNA!=0)]
+tiff("barplot_reads_by_cDNA.tiff",res=300,w=4000,h=2000)
 cols = c("firebrick3","chartreuse4","dodgerblue3","darkorange2","goldenrod")[reads_clones_annot$clin[which(reads_clones_annot$reads_cDNA!=0)]]
 barplot(counts_cDNA,col=cols,main="Number of Reads cDNA",xlab = "Samples", ylab = "Reads",las=2)
+abline(h=100)
 legend(0, 250000, legend=levels(reads_clones_annot$clin[which(reads_clones_annot$reads_cDNA!=0)]),
        col=c("firebrick3","chartreuse4","dodgerblue3","darkorange2","goldenrod"),pch=15, cex=0.8)
-
+dev.off()
 
 ###Plot reads and clones by samples
-par(mfrow = c(2,1))
+
 counts<-table(data_qc$specimen_label,data_qc$amplification_template)
+read_count_ighClones<- unique(data_qc[,c("specimen_label","V_J_lenghCDR3_Clone_igh","amplification_template")])
+counts<-table(read_count_ighClones$specimen_label,read_count_ighClones$amplification_template)
+
+tiff("barplot_reads_clone_by_sample.tiff",res=300,w=4000,h=3000)
+par(mfrow = c(2,1))
 barplot(t(counts),col=c("darkseagreen","steelblue"),main="Number of Reads",xlab = "Samples", ylab = "Reads",
         legend = colnames(counts),args.legend = list(x = "topleft"),las=2) 
 
-read_count_ighClones<- unique(data_qc[,c("specimen_label","V_J_lenghCDR3_Clone_igh","amplification_template")])
-counts<-table(read_count_ighClones$specimen_label,read_count_ighClones$amplification_template)
 barplot(t(counts),col=c("darkgoldenrod3","brown2"),main="Number of Clones",xlab = "Samples", ylab = "Clones",
         legend = colnames(counts),args.legend = list(x = "topleft"),las=2,ylim=c(0,250000))
+dev.off()
 
 ###Plot reads and clones by individuals
 par(mfrow = c(2,1))
@@ -64,6 +74,197 @@ read_count_ighClones<- unique(data_qc[,c("sample_id","V_J_lenghCDR3_Clone_igh","
 counts<-table(read_count_ighClones$sample_id,read_count_ighClones$amplification_template)
 barplot(t(counts),col=c("darkgoldenrod3","brown2"),main="Number of Clones",xlab = "Individuals", ylab = "Clones",
         legend = colnames(counts),args.legend = list(x = "topleft"),las=2,ylim=c(0,800000))
+
+## 1. Clones by Cells
+data_qc_gDNA<-data_qc[which(data_qc$amplification_template=="gDNA"),]
+
+##NP
+specimen_unique_NP<-unique(data_qc_gDNA$specimen_label[which(data_qc_gDNA$clin=="NP")])
+time<-unique(data_qc_gDNA$time[which(data_qc_gDNA$clin=="NP")])
+time<-time[order(time)]
+for (j in 1:length(time)){
+  specimen_unique_time<-unique(data_qc_gDNA$specimen_label[which(data_qc_gDNA$time==time[j])])
+  specimen_unique<-intersect(specimen_unique_time,specimen_unique_NP)
+  #clones<-table(data_qc_gDNA$V_J_lenghCDR3_Clone_igh,data_qc_gDNA$specimen_label)
+  for (i in 1:length(specimen_unique)){
+    data_qc_gDNA_specimen<-data_qc_gDNA[which(data_qc_gDNA$specimen_label==specimen_unique[i]),]
+    if(dim(data_qc_gDNA_specimen)[1]>=100){
+      clones<-table(data_qc_gDNA_specimen$V_J_lenghCDR3_Clone_igh)
+      #write.table(clones,file=paste("NP-time",time[j],"-ind",data_qc_gDNA_specimen$sample_id[i],".txt",sep=""))
+      tiff(paste("NP-time",time[j],"-ind",data_qc_gDNA_specimen$sample_id[i],".tiff",sep=""),width = 170, height = 190)
+      plot(data.matrix(table(clones)),ylim=c(0,1400),xlim=c(0,40),col=c("chartreuse4"),pch=19,xlab = "Number of Cells",ylab = "Number of clones",main = paste("ind=",data_qc_gDNA_specimen$sample_id[1],",time=",data_qc_gDNA_specimen$time[1],sep=""))
+      #ggplot(data.matrix(table(clones)),aes())
+      dev.off()
+    }
+  }
+}
+##PNR
+specimen_unique_PNR<-unique(data_qc_gDNA$specimen_label[which(data_qc_gDNA$clin=="PNR")])
+time<-unique(data_qc_gDNA$time[which(data_qc_gDNA$clin=="PNR")])
+time<-time[order(time)]
+for (j in 1:length(time)){
+  specimen_unique_time<-unique(data_qc_gDNA$specimen_label[which(data_qc_gDNA$time==time[j])])
+  specimen_unique<-intersect(specimen_unique_time,specimen_unique_PNR)
+  for (i in 1:length(specimen_unique)){
+    data_qc_gDNA_specimen<-data_qc_gDNA[which(data_qc_gDNA$specimen_label==specimen_unique[i]),]
+    if(dim(data_qc_gDNA_specimen)[1]>=100){
+      clones<-table(data_qc_gDNA_specimen$V_J_lenghCDR3_Clone_igh)
+      data.matrix(table(clones))
+      tiff(paste("PNR-time",time[j],"-ind",data_qc_gDNA_specimen$sample_id[1],".tiff",sep=""),width = 170, height = 190)
+      plot(data.matrix(table(clones)),ylim=c(0,1400),xlim=c(0,40),col=c("dodgerblue3"),pch=19,xlab = "Number of Cells",ylab = "Number of clones",main = paste("ind=",data_qc_gDNA_specimen$sample_id[1],",time=",data_qc_gDNA_specimen$time[1],sep=""))
+      dev.off()
+    }
+  }
+}
+##PR
+specimen_unique_PR<-unique(data_qc_gDNA$specimen_label[which(data_qc_gDNA$clin=="PR")])
+time<-unique(data_qc_gDNA$time[which(data_qc_gDNA$clin=="PR")])
+time<-time[order(time)]
+for (j in 1:length(time)){
+  specimen_unique_time<-unique(data_qc_gDNA$specimen_label[which(data_qc_gDNA$time==time[j])])
+  specimen_unique<-intersect(specimen_unique_time,specimen_unique_PR)
+  for (i in 1:length(specimen_unique)){
+    data_qc_gDNA_specimen<-data_qc_gDNA[which(data_qc_gDNA$specimen_label==specimen_unique[i]),]
+    if(dim(data_qc_gDNA_specimen)[1]>=100){
+      clones<-table(data_qc_gDNA_specimen$V_J_lenghCDR3_Clone_igh)
+      tiff(paste("PR-time",time[j],"-ind",data_qc_gDNA_specimen$sample_id[1],".tiff",sep=""),width = 170, height = 190)
+      plot(data.matrix(table(clones)),ylim=c(0,1400),col=c("darkorange2"),pch=19,xlim=c(0,40),xlab = "Number of Cells",ylab = "Number of clones",main = paste("ind=",data_qc_gDNA_specimen$sample_id[1],",time=",data_qc_gDNA_specimen$time[1],sep=""))
+      dev.off()
+    }
+  }
+}
+##AR
+specimen_unique_AR<-unique(data_qc_gDNA$specimen_label[which(data_qc_gDNA$clin=="AR")])
+time<-unique(data_qc_gDNA$time[which(data_qc_gDNA$clin=="AR")])
+time<-time[order(time)]
+for (j in 1:length(time)){
+  specimen_unique_time<-unique(data_qc_gDNA$specimen_label[which(data_qc_gDNA$time==time[j])])
+  specimen_unique<-intersect(specimen_unique_time,specimen_unique_AR)
+  for (i in 1:length(specimen_unique)){
+    data_qc_gDNA_specimen<-data_qc_gDNA[which(data_qc_gDNA$specimen_label==specimen_unique[i]),]
+    if(dim(data_qc_gDNA_specimen)[1]>=100){
+      clones<-table(data_qc_gDNA_specimen$V_J_lenghCDR3_Clone_igh)
+      tiff(paste("AR-time",time[j],"-ind",data_qc_gDNA_specimen$sample_id[1],".tiff",sep=""),width = 170, height = 190)
+      plot(data.matrix(table(clones)),ylim=c(0,1400),col=c("firebrick3"),pch=19,xlim=c(0,40),xlab = "Number of Cells",ylab = "Number of clones",main = paste("ind=",data_qc_gDNA_specimen$sample_id[1],",time=",data_qc_gDNA_specimen$time[1],sep=""))
+      dev.off()
+    }
+  }
+}
+
+##preAR
+specimen_unique_preAR<-unique(data_qc_gDNA$specimen_label[which(data_qc_gDNA$clin=="pre-AR")])
+time<-unique(data_qc_gDNA$time[which(data_qc_gDNA$clin=="pre-AR")])
+time<-time[order(time)]
+for (j in 1:length(time)){
+  specimen_unique_time<-unique(data_qc_gDNA$specimen_label[which(data_qc_gDNA$time==time[j])])
+  specimen_unique<-intersect(specimen_unique_time,specimen_unique_preAR)
+  for (i in 1:length(specimen_unique)){
+    data_qc_gDNA_specimen<-data_qc_gDNA[which(data_qc_gDNA$specimen_label==specimen_unique[i]),]
+    if(dim(data_qc_gDNA_specimen)[1]>=100){
+      clones<-table(data_qc_gDNA_specimen$V_J_lenghCDR3_Clone_igh)
+      tiff(paste("pre-AR-time",time[j],"-ind",data_qc_gDNA_specimen$sample_id[1],".tiff",sep=""),width = 170, height = 190)
+      plot(data.matrix(table(clones)),ylim=c(0,1400),col=c("goldenrod"),pch=19,xlim=c(0,40),xlab = "Number of Cells",ylab = "Number of clones",main = paste("ind=",data_qc_gDNA_specimen$sample_id[1],",time=",data_qc_gDNA_specimen$time[1],sep=""))
+      dev.off()
+    }
+  }
+}
+
+
+## 1. Clones by Cells
+data_qc_cDNA<-data_qc[which(data_qc$amplification_template=="cDNA"),]
+
+##NP
+specimen_unique_NP<-unique(data_qc_cDNA$specimen_label[which(data_qc_cDNA$clin=="NP")])
+time<-unique(data_qc_cDNA$time[which(data_qc_cDNA$clin=="NP")])
+time<-time[order(time)]
+for (j in 1:length(time)){
+  specimen_unique_time<-unique(data_qc_cDNA$specimen_label[which(data_qc_cDNA$time==time[j])])
+  specimen_unique<-intersect(specimen_unique_time,specimen_unique_NP)
+  #clones<-table(data_qc_cDNA$V_J_lenghCDR3_Clone_igh,data_qc_cDNA$specimen_label)
+  for (i in 1:length(specimen_unique)){
+    data_qc_cDNA_specimen<-data_qc_cDNA[which(data_qc_cDNA$specimen_label==specimen_unique[i]),]
+    if(dim(data_qc_cDNA_specimen)[1]>=100){
+      clones<-table(data_qc_cDNA_specimen$V_J_lenghCDR3_Clone_igh)
+      #write.table(clones,file=paste("NP-time",time[j],"-ind",data_qc_cDNA_specimen$sample_id[i],".txt",sep=""))
+      tiff(paste("NP-time",time[j],"-ind",data_qc_cDNA_specimen$sample_id[i],".tiff",sep=""),width = 170, height = 190)
+      plot(data.matrix(table(clones)),ylim=c(0,22000),xlim=c(0,200),col=c("chartreuse4"),pch=19,xlab = "Number of Cells",ylab = "Number of clones",main = paste("ind=",data_qc_cDNA_specimen$sample_id[1],",time=",data_qc_cDNA_specimen$time[1],sep=""))
+      #ggplot(data.matrix(table(clones)),aes())
+      dev.off()
+    }
+  }
+}
+##PNR
+specimen_unique_PNR<-unique(data_qc_cDNA$specimen_label[which(data_qc_cDNA$clin=="PNR")])
+time<-unique(data_qc_cDNA$time[which(data_qc_cDNA$clin=="PNR")])
+time<-time[order(time)]
+for (j in 1:length(time)){
+  specimen_unique_time<-unique(data_qc_cDNA$specimen_label[which(data_qc_cDNA$time==time[j])])
+  specimen_unique<-intersect(specimen_unique_time,specimen_unique_PNR)
+  for (i in 1:length(specimen_unique)){
+    data_qc_cDNA_specimen<-data_qc_cDNA[which(data_qc_cDNA$specimen_label==specimen_unique[i]),]
+    if(dim(data_qc_cDNA_specimen)[1]>=100){
+      clones<-table(data_qc_cDNA_specimen$V_J_lenghCDR3_Clone_igh)
+      data.matrix(table(clones))
+      tiff(paste("PNR-time",time[j],"-ind",data_qc_cDNA_specimen$sample_id[1],".tiff",sep=""),width = 170, height = 190)
+      plot(data.matrix(table(clones)),ylim=c(0,22000),xlim=c(0,200),col=c("dodgerblue3"),pch=19,xlab = "Number of Cells",ylab = "Number of clones",main = paste("ind=",data_qc_cDNA_specimen$sample_id[1],",time=",data_qc_cDNA_specimen$time[1],sep=""))
+      dev.off()
+    }
+  }
+}
+##PR
+specimen_unique_PR<-unique(data_qc_cDNA$specimen_label[which(data_qc_cDNA$clin=="PR")])
+time<-unique(data_qc_cDNA$time[which(data_qc_cDNA$clin=="PR")])
+time<-time[order(time)]
+for (j in 1:length(time)){
+  specimen_unique_time<-unique(data_qc_cDNA$specimen_label[which(data_qc_cDNA$time==time[j])])
+  specimen_unique<-intersect(specimen_unique_time,specimen_unique_PR)
+  for (i in 1:length(specimen_unique)){
+    data_qc_cDNA_specimen<-data_qc_cDNA[which(data_qc_cDNA$specimen_label==specimen_unique[i]),]
+    if(dim(data_qc_cDNA_specimen)[1]>=100){
+      clones<-table(data_qc_cDNA_specimen$V_J_lenghCDR3_Clone_igh)
+      tiff(paste("PR-time",time[j],"-ind",data_qc_cDNA_specimen$sample_id[1],".tiff",sep=""),width = 170, height = 190)
+      plot(data.matrix(table(clones)),ylim=c(0,22000),xlim=c(0,200),col=c("darkorange2"),pch=19,xlab = "Number of Cells",ylab = "Number of clones",main = paste("ind=",data_qc_cDNA_specimen$sample_id[1],",time=",data_qc_cDNA_specimen$time[1],sep=""))
+      dev.off()
+    }
+  }
+}
+##AR
+specimen_unique_AR<-unique(data_qc_cDNA$specimen_label[which(data_qc_cDNA$clin=="AR")])
+time<-unique(data_qc_cDNA$time[which(data_qc_cDNA$clin=="AR")])
+time<-time[order(time)]
+for (j in 1:length(time)){
+  specimen_unique_time<-unique(data_qc_cDNA$specimen_label[which(data_qc_cDNA$time==time[j])])
+  specimen_unique<-intersect(specimen_unique_time,specimen_unique_AR)
+  for (i in 1:length(specimen_unique)){
+    data_qc_cDNA_specimen<-data_qc_cDNA[which(data_qc_cDNA$specimen_label==specimen_unique[i]),]
+    if(dim(data_qc_cDNA_specimen)[1]>=100){
+      clones<-table(data_qc_cDNA_specimen$V_J_lenghCDR3_Clone_igh)
+      tiff(paste("AR-time",time[j],"-ind",data_qc_cDNA_specimen$sample_id[1],".tiff",sep=""),width = 170, height = 190)
+      plot(data.matrix(table(clones)),ylim=c(0,22000),xlim=c(0,200),col=c("firebrick3"),pch=19,xlab = "Number of Cells",ylab = "Number of clones",main = paste("ind=",data_qc_cDNA_specimen$sample_id[1],",time=",data_qc_cDNA_specimen$time[1],sep=""))
+      dev.off()
+    }
+  }
+}
+
+##preAR
+specimen_unique_preAR<-unique(data_qc_cDNA$specimen_label[which(data_qc_cDNA$clin=="pre-AR")])
+time<-unique(data_qc_cDNA$time[which(data_qc_cDNA$clin=="pre-AR")])
+time<-time[order(time)]
+for (j in 1:length(time)){
+  specimen_unique_time<-unique(data_qc_cDNA$specimen_label[which(data_qc_cDNA$time==time[j])])
+  specimen_unique<-intersect(specimen_unique_time,specimen_unique_preAR)
+  for (i in 1:length(specimen_unique)){
+    data_qc_cDNA_specimen<-data_qc_cDNA[which(data_qc_cDNA$specimen_label==specimen_unique[i]),]
+    if(dim(data_qc_cDNA_specimen)[1]>=100){
+      clones<-table(data_qc_cDNA_specimen$V_J_lenghCDR3_Clone_igh)
+      tiff(paste("pre-AR-time",time[j],"-ind",data_qc_cDNA_specimen$sample_id[1],".tiff",sep=""),width = 170, height = 190)
+      plot(data.matrix(table(clones)),ylim=c(0,22000),xlim=c(0,200),col=c("goldenrod"),pch=19,xlab = "Number of Cells",ylab = "Number of clones",main = paste("ind=",data_qc_cDNA_specimen$sample_id[1],",time=",data_qc_cDNA_specimen$time[1],sep=""))
+      dev.off()
+    }
+  }
+}
+
+
 
 
 ###Clones longitudional
