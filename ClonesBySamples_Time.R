a0aa48f856dbd57fc760e394ee5d88943783147e
silviapineda <silviapinedasanjuan@gmail.com>
2017-12-21 17:01:24 -0800

    Adding clone matrix definition

--- ClonesBySamples_Time.R
+++ ClonesBySamples_Time.R
@@ -24,6 +24,9 @@ load("/Users/Pinedasans/VDJ/Data/VDJ_order.Rdata")
 ############################################
 #### Study common clones across samples ###
 ##########################################
+
+##gDNA
+
 data_gDNA<-data_qc[which(data_qc$amplification_template=="gDNA"),]
 data_gDNA_long<-data_gDNA[which(data_gDNA$clin!="AR" & data_gDNA$clin!="pre-AR"),]
 
@@ -36,18 +39,19 @@ id<-match(data_gDNA_long$specimen_label,diversity_long_gDNA$specimen_id)
 data_gDNA_long_qc<-data_gDNA_long[which(is.na(id)==F),]
 data_gDNA_long_qc$specimen_label<-factor(data_gDNA_long_qc$specimen_label)
 
-clone_type_matrix<-t(as.data.frame(unclass(table(data_gDNA_long_qc$V_J_lenghCDR3_Clone_igh,factor(data_gDNA_long_qc$specimen_label)))))
-id.spec<-match(rownames(clone_type_matrix),diversity_long_gDNA$specimen_id)
-clone_type_matrix<-cbind(as.character(diversity_long_gDNA$clin[id.spec]),diversity_long_gDNA$time2[id.spec],
-                         diversity_long_gDNA$Sample_id[id.spec],as.character(diversity_long_gDNA$subject_id[id.spec]),clone_type_matrix)
-colnames(clone_type_matrix)[1:4]<-c("clin","time","Sample_id","subject_id")
-clone_type_df<-as.data.frame(clone_type_matrix)
+clone_type_matrix_gDNA<-t(as.data.frame(unclass(table(data_gDNA_long_qc$V_J_lenghCDR3_Clone_igh,factor(data_gDNA_long_qc$specimen_label)))))
+id.spec<-match(rownames(clone_type_matrix_gDNA),diversity_long_gDNA$specimen_id)
+clone_type_matrix_gDNA<-cbind(as.character(diversity_long_gDNA$clin[id.spec]),diversity_long_gDNA$time2[id.spec],
+                              diversity_long_gDNA$Sample_id[id.spec],as.character(diversity_long_gDNA$subject_id[id.spec]),clone_type_matrix_gDNA)
+colnames(clone_type_matrix_gDNA)[1:4]<-c("clin","time","Sample_id","subject_id")
+clone_type_df_gDNA<-as.data.frame(clone_type_matrix_gDNA)
 
-clone_type_matrix_num<-clone_type_matrix[,5:ncol(clone_type_matrix)]
-clone_type_matrix_num2<-t(apply(clone_type_matrix_num,1,as.numeric))
-colnames(clone_type_matrix_num2)<-colnames(clone_type_matrix[,5:ncol(clone_type_matrix)])
+clone_type_matrix_num_gDNA<-clone_type_matrix_gDNA[,5:ncol(clone_type_matrix_gDNA)]
+clone_type_matrix_num2_gDNA<-t(apply(clone_type_matrix_num_gDNA,1,as.numeric))
+colnames(clone_type_matrix_num2_gDNA)<-colnames(clone_type_matrix_gDNA[,5:ncol(clone_type_matrix_gDNA)])
 
-clone_type_matrix_reduced<-clone_type_matrix_num2[,which(colSums(clone_type_matrix_num2)!=0)]
+clone_type_matrix_reduced_gDNA<-clone_type_matrix_num2_gDNA[,which(colSums(clone_type_matrix_num2_gDNA)!=0)]
+save(clone_type_df_gDNA,clone_type_matrix_reduced_gDNA,diversity_long_gDNA,file="~/VDJ/Data/clonesBySamples_gDNA.Rdata")
 
 
 ###To measure which individuals have clones across data points
@@ -98,3 +102,96 @@ for (i in 1:length(sample_filter)){
 
 names(persistance)<-subject_filter
 write.csv(persistance,file="persistance.csv")
+
+
+###cDNA
+data_cDNA<-data_qc[which(data_qc$amplification_template=="cDNA"),]
+data_cDNA_long<-data_cDNA[which(data_cDNA$clin!="AR" & data_cDNA$clin!="pre-AR"),]
+
+
+diversity<-read.csv("/Users/Pinedasans/VDJ/Data/diversity.csv",header=T)
+diversity_cDNA_qc<-diversity[which(diversity$reads_cDNA>=100),]
+diversity_long_cDNA<-diversity_cDNA_qc[which(diversity_cDNA_qc$clin=="NP" | diversity_cDNA_qc$clin=="PNR" | diversity_cDNA_qc$clin=="PR"),]
+diversity_long_cDNA$clin<-factor(diversity_long_cDNA$clin, levels=c("NP", "PNR", "PR"))
+
+id<-match(data_cDNA_long$specimen_label,diversity_long_cDNA$specimen_id)
+data_cDNA_long_qc<-data_cDNA_long[which(is.na(id)==F),]
+data_cDNA_long_qc$specimen_label<-factor(data_cDNA_long_qc$specimen_label)
+
+clone_type_matrix_cDNA<-t(as.data.frame(unclass(table(data_cDNA_long_qc$V_J_lenghCDR3_Clone_igh,factor(data_cDNA_long_qc$specimen_label)))))
+id.spec<-match(rownames(clone_type_matrix_cDNA),diversity_long_cDNA$specimen_id)
+clone_type_matrix_cDNA<-cbind(as.character(diversity_long_cDNA$clin[id.spec]),diversity_long_cDNA$time2[id.spec],
+                         diversity_long_cDNA$Sample_id[id.spec],as.character(diversity_long_cDNA$subject_id[id.spec]),clone_type_matrix_cDNA)
+colnames(clone_type_matrix_cDNA)[1:4]<-c("clin","time","Sample_id","subject_id")
+clone_type_df_cDNA<-as.data.frame(clone_type_matrix_cDNA)
+
+clone_type_matrix_num_cDNA<-clone_type_matrix_cDNA[,5:ncol(clone_type_matrix_cDNA)]
+clone_type_matrix_num2_cDNA<-t(apply(clone_type_matrix_num_cDNA,1,as.numeric))
+colnames(clone_type_matrix_num2_cDNA)<-colnames(clone_type_matrix_cDNA[,5:ncol(clone_type_matrix_cDNA)])
+
+clone_type_matrix_reduced_cDNA<-clone_type_matrix_num2_cDNA[,which(colSums(clone_type_matrix_num2_cDNA)!=0)]
+
+save(clone_type_df_cDNA,clone_type_matrix_reduced_cDNA,diversity_long_cDNA,file="~/VDJ/Data/clonesBySamples_cDNA.Rdata")
+
+
+#################################
+### common clones by samples ###
+###############################
+load("~/VDJ/Data/clonesBySamples_gDNA.Rdata")
+load("~/VDJ/Data/clonesBySamples_cDNA.Rdata")
+
+specimen<-rownames(clone_type_matrix_reduced_gDNA)
+common<-matrix(NA,length(specimen),length(specimen))
+for (i in 1:length(specimen)){
+  for (j in 1:length(specimen)){
+    #gDNA
+    clone_type_gDNA_reduced_specimen<-clone_type_matrix_reduced_gDNA[which(rownames(clone_type_matrix_reduced_gDNA)==specimen[i]),]
+    clone_type_gDNA_reduced_specimen_present<-clone_type_gDNA_reduced_specimen[which(clone_type_gDNA_reduced_specimen!=0)]
+    
+    #cDNA
+    clone_type_cDNA_reduced_specimen<-clone_type_matrix_reduced_cDNA[which(rownames(clone_type_matrix_reduced_cDNA)==specimen[j]),]
+    clone_type_cDNA_reduced_specimen_present<-clone_type_cDNA_reduced_specimen[which(clone_type_cDNA_reduced_specimen!=0)]
+    
+    common[i,j]<-length(intersect(names(clone_type_gDNA_reduced_specimen_present),
+                                  names(clone_type_cDNA_reduced_specimen_present)))
+  }
+}
+write.csv(common,"common.csv")
+common<-read.csv("common.csv")
+colnames(common)<-specimen
+rownames(common)<-specimen
+
+common2<-common[which(colSums(common)!=0),]
+common3<-common2[,which(colSums(common2)!=0)]
+
+subject<-factor(diversity_long_gDNA[match(colnames(common3),diversity_long_gDNA$specimen_id),"subject_id"])
+clones_gDNA<-diversity_long_gDNA[match(colnames(common3),diversity_long_gDNA$specimen_id),"clones_gDNA"]
+clones_common_perc<-common3/clones_gDNA
+colnames(clones_common_perc)<-subject
+rownames(clones_common_perc)<-subject
+#Plot the matrix
+xval <- format(clones_common_perc, format="f", digits=2)
+pal <- colorRampPalette(c(rgb(0.96,0.96,1), rgb(0.1,0.1,0.9)), space = "rgb")
+
+library(gplots)
+heatmap.2(as.matrix(clones_common_perc), Rowv=FALSE, Colv=FALSE, dendrogram="none", xlab="Columns", ylab="Rows", col=pal,cellnote=xval,
+          tracecol="#303030", trace="none", notecol="black", 
+          notecex=0.8, keysize = 1.5, margins=c(5, 5))
+
+common_diag<-diag(as.matrix(common3))
+common_diag_freq<-common_diag/clones_gDNA
+
+COLOR=c("chartreuse4", "dodgerblue3","darkorange2")
+names(common_diag_freq)<-subject
+clin<-diversity_long_gDNA[match(names(common_diag_freq),diversity_long_gDNA$subject_id),"clin"]
+barplot(common_diag_freq[order(common_diag_freq,decreasing = T)],col = COLOR[clin[order(common_diag_freq,decreasing = T)]], cex.names=0.8,las=2)
+
+time<-diversity_long_gDNA[match(names(common_diag_freq),diversity_long_gDNA$subject_id),"time2"]
+
+time_2<-time[which(time==6 | time==24)]
+common_diag_freq_2<-common_diag_freq[which(time==6 | time==24)]
+summary(glm(common_diag_freq_2~factor(time_2)))
+library("RColorBrewer")
+color<-brewer.pal(6,"Set1")
+boxplot(common_diag_freq_2~factor(time_2),col=color[c(1,4)])
+
