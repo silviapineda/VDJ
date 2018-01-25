50dce284e2f41c3c410474785734222a02a45b09
silviapineda <silviapinedasanjuan@gmail.com>
2018-01-24 17:34:40 -0800

    Analysis at clone level and plots to show the selected

--- Clone_type_Analysis_cDNA.R
+++ Clone_type_Analysis_cDNA.R
@@ -25,33 +25,31 @@ library(pheatmap)
 ##################
 ### Analysis in the clone data matrix
 #################
-setwd("/Users/Pinedasans/VDJ/SummaryResults/AllClones/")
-load("~/VDJ/Data/clonesInferedAll_cDNA_90.Rdata")
-clone_distribution<-colSums(clone_type_cDNA_num_reduced)
+setwd("/Users/Pinedasans/VDJ/ResultsAllClones//")
+load("~/VDJ/Data/clones_cDNA.Rdata")
+
+###Filter by clones that at least are share in 10% of the samples
+clone_type_cDNA_num_filter<-clone_type_cDNA_num_reduced[,colSums(clone_type_cDNA_num_reduced!=0)>5] #3,795
+
+##Distribution of clones
+clone_distribution<-colSums(clone_type_cDNA_num_filter)
 clone_distribution[order(clone_distribution,decreasing = T)]
 
-###Filter out by clone distribution [all clones that only have 1 BCR in 1 sample]
-clone_distribution_filter<-clone_distribution[which(clone_distribution>1)] 
-
-barplot(clone_distribution_filter[order(clone_distribution_filter,decreasing = T)])
+##Top 20 clones
+barplot(clone_distribution[order(clone_distribution,decreasing = T)])
 
 ###Example clone with the maximum levels
-table(data_cDNA_long_qc[grep("IGHV3-30_IGHJ5_42_1674",data_cDNA_long_qc$CloneId2),c("clin","time")])
-table(data_cDNA_long_qc[grep("IGHV3-30_IGHJ5_42_1674",data_cDNA_long_qc$CloneId2),c("specimen_label")])
+table(data_cDNA_long[grep("IGHV4-59_IGHJ4_30_4700",data_cDNA_long$V_J_lenghCDR3_CloneId),c("clin","time")])
+table(data_cDNA_long[grep("IGHV4-39_IGHJ5_51_803",data_cDNA_long$V_J_lenghCDR3_CloneId),c("specimen_label")])
 
-####Filter the clone matrix to perform the analysis
-id<-match(names(clone_distribution_filter),colnames(clone_type_cDNA_num_reduced))
-clone_type_cDNA_num_filter<-clone_type_cDNA_num_reduced[,id]
-clone_type_cDNA_num_filter2<-clone_type_cDNA_num_filter[,colSums(clone_type_cDNA_num_filter!=0)>3] ##194 clones
-
-matrix_clones<-cbind(clone_type_cDNA_df[,2],clone_type_cDNA_num_filter2)
+matrix_clones<-cbind(clone_type_cDNA_df[,2],clone_type_cDNA_num_filter)
 set.seed(112233)
 fit<-VSURF(x = matrix_clones, y=factor(clone_type_cDNA_df[,1]),parallel = TRUE,ncores=4)
 clones<-data.frame(matrix_clones[,fit$varselect.interp])
 
 set.seed(1000)
 rf_output <- randomForest(factor(clone_type_cDNA_df[,1])~.,data=clones,proximity=TRUE, keep.forest=T,ntree=1000)
-tiff("MDSplot_cloneType_RF.tiff",res=300,w=2000,h=2000)
+tiff("MDSplot_cloneType_RF_cDNA.tiff",res=300,w=2000,h=2000)
 clone_type_cDNA_df$timeplot<-ifelse(clone_type_cDNA_df$time==0,1,
                                            ifelse(clone_type_cDNA_df$time==6,1.5,2))
 MDSplot(rf_output, factor(clone_type_cDNA_df$clin),cex=clone_type_cDNA_df$timeplot,
@@ -76,7 +74,7 @@ individuals<-paste0("individual",unlist(lapply(numbers, `[[`, 2)))
 
 annotation_col = data.frame(
   individual = factor(individuals),
-  time = as.factor(clone_type_cDNA_df_filter$time),
+  time = factor(clone_type_cDNA_df_filter$time),
   clin = clone_type_cDNA_df_filter$clin)
 
 
@@ -89,56 +87,43 @@ ann_colors = list (clin = c("NP" = fill[1], "PNR" = fill[2], "PR" = fill[3]),
                    time = c("0" = fill2[1],"6" = fill2[2], "24" = fill2[3]),  
                    individual= c("individual1" = fill3[1],"individual2" = fill3[2],"individual3"= fill3[3],"individual4"= fill3[4],"individual5"= fill3[5],"individual6"= fill3[6],
                                  "individual7"= fill3[7],"individual8"= fill3[8],"individual9"= fill3[9],"individual10"=fill3[10], "individual11"=fill3[11]  ,"individual12"=fill3[12], "individual13"=fill3[13] ,
-                                 "individual14"=  fill3[14], "individual16"= fill3[15],"individual17"= fill3[16],"individual18"= fill3[17],"individual19"= fill3[18],"individual20"= fill3[19],
-                                 "individual21"= fill3[20], "individual22"= fill3[21], "individual23"= fill3[22], "individual24"= fill3[23], "individual25"= fill3[24], "individual26"=fill3[25]))
-
+                                 "individual14"=  fill3[14],"individual15"=  fill3[15], "individual16"= fill3[16],"individual17"= fill3[17],"individual18"= fill3[18],"individual19"= fill3[19],"individual20"= fill3[20],
+                                 "individual21"= fill3[21], "individual22"= fill3[22], "individual23"= fill3[23], "individual24"= fill3[24], "individual25"= fill3[25], "individual26"=fill3[26],"individual27"=fill3[27]))
 
 colfunc<-colorRampPalette(c("white","red4"))
-tiff("heatmap_cloneType_RF.tiff",res=300,w=3500,h=2500)
+
+tiff("heatmap_cloneType_RF_cDNA.tiff",res=300,w=3500,h=2500)
 pheatmap(t(clones),cex=1.0,annotation_col = annotation_col,
-         annotation_colors = ann_colors,rownames = F,color =  colfunc(100),border_color=F)
+         annotation_colors = ann_colors,rownames = F,color =  colfunc(1000),border_color=F)
 dev.off()
 
-prefix<-"clone"
-sufix<-seq(1,dim(clone_type_cDNA_filter2)[2])
-my_names<-paste(prefix,sufix,sep=".")
-colnames(clone_type_cDNA_filter2)<-my_names
-paste(my_names,collapse ="+")
+write.csv(clones,file="clones_cDNA_RF.csv")
+table(data_cDNA_long[grep(colnames(clones)[1],data_cDNA_long$V_J_lenghCDR3_CloneId),c("clin","time")])
 
-matrix_clones<-cbind(clone_type_cDNA_df_filter[,1:4],clone_type_cDNA_filter2)
-paste(my_names,collapse ="+")
-
-library(glmmLasso)
-lm1 <- glmmLasso(clin ~ clone.1+clone.2+clone.3+clone.4+clone.5+clone.6+clone.7+clone.8+clone.9+clone.10+
-                   clone.11+clone.12+clone.13+clone.14+clone.15+clone.16+clone.17+clone.18+clone.19+clone.20
-                 +clone.21+clone.22+clone.23+clone.24+clone.25+clone.26+clone.27+clone.28+clone.29+clone.30+
-                   clone.31+clone.32+clone.33+clone.34+clone.35+clone.36+clone.37+clone.38+clone.39+clone.40+
-                   clone.41+clone.42+clone.43+clone.44+clone.45+clone.46+clone.47+clone.48+clone.49+clone.50+
-                   clone.51+clone.52+clone.53+clone.54+clone.55+clone.56+clone.57+clone.58+clone.59+clone.60+
-                   clone.61+clone.62+clone.63+clone.64+clone.65+clone.66+clone.67+clone.68+clone.69+clone.70+
-                   clone.71+clone.72+clone.73, family = acat(),
-                 rnd = list(time=~1+Sample_id),switch.NR=TRUE,control=list(print.iter=TRUE),
-                 lambda=10,data=matrix_clones)
-library(corrplot)
-corrplot(cor(clone_type_cDNA_filter2))
-
-p_value=NULL
-for (i in 59:dim(matrix_clones)[2]){
+id<-NULL
+for(i in 1:31){
   print(i)
-  fm_null <- lmer(matrix_clones[,i] ~ matrix_clones$clin + matrix_clones$time + (1 | Sample_id),data=matrix_clones,REML = F)
-  fm_full <- lmer(matrix_clones[,i]  ~  matrix_clones$clin*matrix_clones$time + (1 | Sample_id) ,data=matrix_clones,REML = F)
-  p_value[i]<-anova(fm_full, fm_null)[2,8]
+  id<-c(id,grep(colnames(clones)[i],data_cDNA_long$V_J_lenghCDR3_CloneId))
 }
+data_cDNA_long_clone<-data_cDNA_long[id,]
+data_cDNA_long_clone$clin<-factor(data_cDNA_long_clone$clin)
+data_cDNA_long_clone_barplot<-as.data.frame(table(data_cDNA_long_clone[,c("V_J_lenghCDR3_CloneId","sample_id","clin")]))
 
-p <- ggplot(fm_full, aes(x = time, y =matrix_clones[,i], colour = clin)) +
-  scale_colour_manual(values=c("chartreuse4", "dodgerblue3","darkorange2")) +
-  geom_point(size=3) +
-  geom_smooth(method="lm",size=1.5) +
-  labs(x = "time (months)",y = "Clones") 
+data_cDNA_long_clone_barplot$clin<-factor(data_cDNA_long_clone_barplot$clin)
+data_cDNA_long_clone_barplot<-data_cDNA_long_clone_barplot[which(data_cDNA_long_clone_barplot$Freq!=0),]
+data_cDNA_long_clone_barplot<-data_cDNA_long_clone_barplot[order(data_cDNA_long_clone_barplot$Freq),]
 
-print(p)
+tiff("IndividualClonesRF.tiff",res=300,w=4000,h=2000)
+ggplot(data_cDNA_long_clone_barplot[order(data_cDNA_long_clone_barplot$clin),], aes(V_J_lenghCDR3_CloneId, y=Freq,fill=clin)) +
+  geom_bar(stat="identity",colour="black") + scale_fill_manual(values=c("chartreuse4","dodgerblue3","darkorange2")) + 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))+ geom_col(position = position_stack(reverse = TRUE),colour="black")
+dev.off()
 
 library(gamlr)
-fit <- gamlr(matrix_clones[,5:ncol(matrix_clones)], matrix_clones$clin, gamma=10, lambda.min.ratio=0.1,
-             standardize=FALSE, family="binomial")
+x<-data.matrix(clone_type_cDNA_num_filter)
+y<-clone_type_cDNA_df[,1]
+y<-factor(replace(y,y=="PNR","PR"))
+cvfit <- cv.gamlr(x, y,family="binomial" , gamma=1, verb=TRUE)
+
+fitlasso <- gamlr(x, y,family="binomial" , gamma=1, lambda.min.ratio=cvfit$lambda.min)
 
